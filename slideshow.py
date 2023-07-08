b44e8ca2dfdec2d2c3f4e6447f1d2ee3122a36b8
jasonm23 <jasonm23@gmail.com>
2023-07-08 15:22:23 +0700

    Amend usage info

--- slideshow.py
+++ slideshow.py
@@ -318,20 +318,23 @@ if __name__ == '__main__':
 
         Control the slideshow:
 
+        Keyboard:
         Esc,q - quit
         [, ] - change image delay time
         1-9 - change image delay time (number to seconds)
         f - maximize window (fullscreen)
         r - random toggle
         k - Ken Burns effect toggle
+        i - Copy image filename to clipboard
         SPACE - pause/resume
         left, right - move between images
-        mouse left click - move between images (click on left or right side)
 
-        mouse right click, on screen 3rds
-          left side - random/ordered
-          middle - pause/resume
-          right side - Ken Burns effect toggle
+        Mouse:
+        Left click - move between images (click on left or right side)
+        Right click
+          left side (1/3) - random/ordered
+          middle (1/3) - pause/resume
+          right side (1/3) - Ken Burns effect toggle
 
         """)
 
